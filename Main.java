e24b948688d79622eadc5ca7779d939d55e109c7
olegbc <olegkamuz@gmail.com>
2018-01-08 15:56:02 +0200

    1.1.3 three inputs equality

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int one = Integer.parseInt(args[0]);
+        int two = Integer.parseInt(args[1]);
+        int three = Integer.parseInt(args[2]);
+        if (one == two && two == three){
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+
+}
