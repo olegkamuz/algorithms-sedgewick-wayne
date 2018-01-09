98459eba294187c3aaaa9f2f9c4b0f0dcd693007
olegbc <olegkamuz@gmail.com>
2018-01-09 04:48:12 +0200

    1.1.12 mirror array

--- Main.java
+++ Main.java
@@ -1,14 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        String[][] arr = new String[10][10];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (StdRandom.bernoulli(0.5)) arr[i][j] = "" + i + j +"|*";
-                else arr[i][j] = "" + i + j + "| ";
-                System.out.print(arr[i][j]);
-            }
-            System.out.println();
-        }
+        int[] a = new int[10];
+        for (int i = 0; i < 10; i++)
+            a[i] = 9 - i;
+        for (int i = 0; i < 10; i++)
+            a[i] = a[a[i]];
+        for (int i = 0; i < 10; i++)
+            System.out.println(a[i]);
     }
 }
