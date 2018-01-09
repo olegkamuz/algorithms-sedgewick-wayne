bc3e110104c524ceac365ba91e858b7dc539f71b
olegbc <olegkamuz@gmail.com>
2018-01-09 04:32:13 +0200

    1.1.11 multi array of true(*) and false( )

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        String s = "";
-        for (int n = 6; n > 0; n /= 2)
-            s = (n % 2) + s;
-        System.out.println(s);
+        String[][] arr = new String[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (StdRandom.bernoulli(0.5)) arr[i][j] = "" + i + j +"|*";
+                else arr[i][j] = "" + i + j + "| ";
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
