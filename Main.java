cd8bede23dcce4d0a8f59456a35c47139fd7107d
olegbc <olegkamuz@gmail.com>
2018-01-09 02:06:39 +0200

    1.1.7 b

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        double t = 9.0;
-        while (Math.abs(t - 9.0/t) > .001)
-            t = (9.0/t + t) / 2.0;
-        StdOut.printf("%.5f\n", t);
+        int sum = 0;
+        for (int i = 1; i < 1000; i++)
+            for (int j = 0; j < i; j++)
+                sum++;
+        StdOut.println(sum);
     }
 }
