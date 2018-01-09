34bf26628a86d06f53fd8f6ecaf973db8abf2cd0
olegbc <olegkamuz@gmail.com>
2018-01-09 16:36:10 +0200

    1.1.13 multi array transposition

--- Main.java
+++ Main.java
@@ -1,12 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] a = new int[10];
-        for (int i = 0; i < 10; i++)
-            a[i] = 9 - i;
-        for (int i = 0; i < 10; i++)
-            a[i] = a[a[i]];
-        for (int i = 0; i < 10; i++)
-            System.out.println(a[i]);
+        String[][] arrPre = new String[10][10];
+        String[][] arrPost = new String[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++){
+                arrPre[i][j] = " " + i + j + " ";
+                System.out.print(arrPre[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++){
+                arrPost[i][j] = arrPre[j][i];
+                System.out.print(arrPost[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
