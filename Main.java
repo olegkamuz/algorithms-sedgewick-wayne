d28d55f0c9b549ef6b6d166cd2c21d746b7cf4a4
olegbc <olegkamuz@gmail.com>
2018-01-09 02:32:55 +0200

    1.1.8

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        int sum = 0;
-        for (int i = 1; i < 1000; i *= 2)
-            for (int j = 0; j < i; j++)
-                sum++;
-        StdOut.println(sum);
+        System.out.println('b');
+        System.out.println('b' + 'c');
+        System.out.println((char) ('a' + 4));
     }
 }
