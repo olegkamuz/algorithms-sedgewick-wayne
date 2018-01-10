321939bab843b3541c2326b51d4ce32c718c5df7
olegbc <olegkamuz@gmail.com>
2018-01-11 01:53:29 +0200

    1.1.16 exR1(6)

--- Main.java
+++ Main.java
@@ -1,26 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] a = {5, 4, 1, 1, 0, 4, 21, 1, 2, 1};
-        int[] histogram = histogram(a, 6);
-        for (int i = 0; i < histogram.length; i++) {
-            System.out.println("" + i + " : " + histogram[i]);
-        }
+        System.out.println(exR1(6));
     }
 
-    public static int[] histogram(int[] a, int M) {
-        int[] hist = new int[M];
-        for(int i = 0; i < M; i++){
-            hist[i] = searchAndCount(a, i);
-        }
-        return hist;
-    }
-
-    public static int searchAndCount(int[] a, int needle) {
-        int count = 0;
-        for(int i = 0; i < a.length; i++){
-            if (a[i] == needle) count++;
-        }
-        return count;
+    public static String exR1(int n)
+    {
+        if (n <= 0) return "";
+        return exR1(n-3) + n + exR1(n-2) + n;
     }
 }
