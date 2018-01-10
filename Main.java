7b3a519f98fe0fe5e2e1564f788f0d91d222ddb0
olegbc <olegkamuz@gmail.com>
2018-01-10 07:26:45 +0200

    1.1.15 histogram

--- Main.java
+++ Main.java
@@ -1,35 +1,26 @@
 public class Main {
 
     public static void main(String[] args) {
+        int[] a = {5, 4, 1, 1, 0, 4, 21, 1, 2, 1};
+        int[] histogram = histogram(a, 6);
+        for (int i = 0; i < histogram.length; i++) {
+            System.out.println("" + i + " : " + histogram[i]);
+        }
     }
 
-    public static int binSearch(int[] a, int i) {
-        int hi = a.length;
-        int low = 0;
-        int mid;
-        while (low < hi) {
-            mid = (low - hi)/2;
-            if (a[mid] == i) return i;
-            if(i > a[mid]) {
-                low = mid + 1;
-            }else{
-                hi = mid - 1;
-            }
+    public static int[] histogram(int[] a, int M) {
+        int[] hist = new int[M];
+        for(int i = 0; i < M; i++){
+            hist[i] = searchAndCount(a, i);
         }
-        return -1;
+        return hist;
     }
 
-    public static int[] quicksort(int[] a){
-        int temp;
-        for(int j = 0; j < a.length; j++){
-            for(int i = 0; i < a.length; i++){
-                if (a[j] < a[i]) {
-                    temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
-                }
-            }
+    public static int searchAndCount(int[] a, int needle) {
+        int count = 0;
+        for(int i = 0; i < a.length; i++){
+            if (a[i] == needle) count++;
         }
-        return a;
+        return count;
     }
 }
