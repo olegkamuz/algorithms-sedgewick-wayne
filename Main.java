a14d5c6e33383d56a1f280fed419be0744a6da7f
olegbc <olegkamuz@gmail.com>
2018-01-11 06:22:57 +0200

    1.1.19 Fibonacci

--- Main.java
+++ Main.java
@@ -1,12 +1,21 @@
 public class Main {
-
-    public static void main(String[] args) {
-        System.out.println(exR1(6));
+    public static long[] F(int N) {
+        long[] FibRow = new long[N];
+        FibRow[0] = 0;
+        if(N == 0) return FibRow;
+        FibRow[1] = 1;
+        if(N == 1) return FibRow;
+        for (int i = 2; i < N; i++) {
+            FibRow[i] = FibRow[i-1] + FibRow[i-2];
+        }
+        return FibRow;
     }
 
-    public static String exR1(int n)
-    {
-        if (n <= 0) return "";
-        return exR1(n-3) + n + exR1(n-2) + n;
+    public static void main(String[] args) {
+        int N = 100;
+        long[] arr = F(N);
+        for (int i = 0; i < N; i++) {
+            StdOut.println(i + " : " +arr[i]);
+        }
     }
 }
