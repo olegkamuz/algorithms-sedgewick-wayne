1430fe0c3871ef5afa9f27f1eca0fb3bfc6f009b
olegbc <olegkamuz@gmail.com>
2018-01-12 04:11:56 +0200

    1.1.21 table of name and values

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(factorial(14));
-    }
-
-    public static long factorial(int N) {
-        if (N == 1) return 1;
-        return N * factorial(N - 1);
+        In in = new In(args[0]);
+        String[] whitelist = in.readAllLines();
+        for(int i = 0; i < whitelist.length; i++){
+            String[] arr = whitelist[i].split(" ");
+            System.out.printf("%-14s%-14d%-14d%-14.3f", arr[0], Integer.parseInt(arr[1]), Integer.parseInt(arr[2]), Double.parseDouble(arr[1])/Double.parseDouble(arr[2]));
+            System.out.println();
+        }
     }
 }
 
