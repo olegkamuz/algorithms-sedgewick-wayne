64eee58fcbfae9c8eb04d7a96ad5e96413111434
olegbc <olegkamuz@gmail.com>
2018-01-30 19:43:37 +0200

    Exercise1218_VarianceForAccumulator

---
+++ Exercise1218_VarianceForAccumulator.java
@@ -0,0 +1,33 @@
+public class Exercise1218_VarianceForAccumulator {
+    private double m;
+    private double s;
+    private int N;
+    public void addDataValue(double x)
+    {
+        N++;
+        s = s + 1.0 * (N-1) / N * (x - m) * (x - m);
+        m = m + (x - m) / N;
+    }
+    public double mean()
+    { return m; }
+    public double var()
+    { return s/(N - 1);
+    }
+    public double stddev()
+    { return Math.sqrt(this.var());
+    }
+    public static void main(String[] args){
+        Exercise1218_VarianceForAccumulator varAcc = new Exercise1218_VarianceForAccumulator();
+        varAcc.addDataValue(1.0);
+        varAcc.addDataValue(4.0);
+        varAcc.addDataValue(3.0);
+        varAcc.addDataValue(7.0);
+        varAcc.addDataValue(3.0);
+        varAcc.addDataValue(2.0);
+        varAcc.addDataValue(3.0);
+        varAcc.addDataValue(5.0);
+        varAcc.addDataValue(4.0);
+        varAcc.addDataValue(2.0);
+        System.out.printf("mean: %1f variance: %1f", varAcc.mean(), varAcc.var());
+    }
+}
