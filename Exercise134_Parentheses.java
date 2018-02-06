b03c7435af61f8a6c5a714eb768db60972528d8d
olegbc <olegkamuz@gmail.com>
2018-02-06 04:01:39 +0200

    isBalanced refactoring

--- Exercise134_Parentheses.java
+++ Exercise134_Parentheses.java
@@ -5,27 +5,22 @@ public class Exercise134_Parentheses {
         printCheck(isBalanced(string1), string1);
         printCheck(isBalanced(string2), string2);
     }
-    private static boolean isBalanced(String string){
+
+    private static boolean isBalanced(String string) {
         String[] arr = string.split("");
         Stack<String> stackOpen = new Stack<String>();
-        for(String s: arr){
-            if (s.equals("]")) {
-                if (!stackOpen.isEmpty() && stackOpen.pop().equals("["));
-                else return false;
-            } else if (s.equals("}")) {
-                if(!stackOpen.isEmpty() && stackOpen.pop().equals("{"));
-                else return false;
-            } else if (s.equals(")")) {
-               if(!stackOpen.isEmpty() && stackOpen.pop().equals("("));
-                else return false;
-            } else {
-                stackOpen.push(s);
-            }
+        for (String s : arr) {
+            if (s.equals("[") || s.equals("{") || s.equals("(")) stackOpen.push(s);
+            else if (stackOpen.isEmpty()) return false;
+            else if (s.equals("]") && !stackOpen.pop().equals("[")) return false;
+            else if (s.equals("}") && !stackOpen.pop().equals("{")) return false;
+            else if (s.equals(")") && !stackOpen.pop().equals("(")) return false;
         }
         if (!stackOpen.isEmpty()) return false;
         return true;
     }
-    private static void printCheck(boolean isBalanced, String string){
+
+    private static void printCheck(boolean isBalanced, String string) {
         if (isBalanced) {
             System.out.printf("String %1s is balanced", string);
         } else {
