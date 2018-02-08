3a1efb08959b5b15da5093470ede5e398d76bc35
olegbc <olegkamuz@gmail.com>
2018-02-08 15:06:25 +0200

    Exercise1311_EvaluatePostfix

--- Exercise1310_InfixToPostfix.java
+++ Exercise1310_InfixToPostfix.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
-
 public class Exercise1310_InfixToPostfix {
+    public static void main(String[] args) {
+        Stack<String> ops = new Stack<String>();
+        Stack<String> stringsStack = new Stack<String>();
+        String string = "1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) )";
+        String[] arr = string.split(" ");
+        for (String s: arr) {
+            if (s.equals("("));
+            else if (s.equals("+"))ops.push(s);
+            else if (s.equals("-"))ops.push(s);
+            else if (s.equals("*"))ops.push(s);
+            else if (s.equals("/"))ops.push(s);
+            else if (s.equals(")")) {
+                String op = ops.pop();
+                if (op.equals("+")) {
+                    String rightOperand = stringsStack.pop();
+                    String leftOperand = stringsStack.pop();
+                    stringsStack.push("( " + leftOperand + " " + rightOperand + " + ) ") ;
+                }
+                else if (op.equals("-")) {
+                    String rightOperand = stringsStack.pop();
+                    String leftOperand = stringsStack.pop();
+                    stringsStack.push("( " + leftOperand + " " + rightOperand + " - ) ") ;
+                }
+                else if (op.equals("*")){
+                    String rightOperand = stringsStack.pop();
+                    String leftOperand = stringsStack.pop();
+                    stringsStack.push("( " + leftOperand + " " + rightOperand + " * ) ") ;
+                }
+                else if (op.equals("/")){
+                    String rightOperand = stringsStack.pop();
+                    String leftOperand = stringsStack.pop();
+                    stringsStack.push("( " + leftOperand + " " + rightOperand + " / ) ") ;
+                }
+            } else {
+                stringsStack.push(s);
+            }
+        }
+        for (String s: stringsStack){
+            StdOut.print(s);
+        }
+    }
 }
---
+++ Exercise1311_EvaluatePostfix.java
@@ -0,0 +1,37 @@
+public class Exercise1311_EvaluatePostfix {
+    public static void main(String[] args) {
+        Stack<String> ops = new Stack<String>();
+        Stack<Double> vals = new Stack<Double>();
+        while (!StdIn.isEmpty())
+        {
+            String s = StdIn.readString();
+            if (s.equals("("));
+            else if (s.equals("+"))
+                ops.push(s);
+            else if (s.equals("-"))
+                ops.push(s);
+            else if (s.equals("*"))
+                ops.push(s);
+            else if (s.equals("/"))
+                ops.push(s);
+            else if (s.equals("sqrt")) ops.push(s);
+            else if (s.equals(")"))
+            {
+                String op = ops.pop();
+                double v = vals.pop();
+                if (op.equals("+"))
+                    v = vals.pop() + v;
+                else if (op.equals("-"))
+                    v = vals.pop() - v;
+                else if (op.equals("*"))
+                    v = vals.pop() * v;
+                else if (op.equals("/"))
+                    v = vals.pop() / v;
+                else if (op.equals("sqrt")) v = Math.sqrt(v);
+                vals.push(v);
+            }
+            else vals.push(Double.parseDouble(s));
+        }
+        StdOut.println(vals.pop());
+    }
+}
