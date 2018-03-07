25f8652ac14deef175f72bcf87dd39d46a368181
olegbc <olegkamuz@gmail.com>
2018-03-07 17:02:09 +0200

    Exercise1335_RandomeQueue two test cases

--- Exercise1335_RandomQueue.java
+++ Exercise1335_RandomQueue.java
@@ -3,6 +3,15 @@ public class Exercise1335_RandomQueue {
         MixedDeck mixedDeck = MixedDeck.MixedDeck();
         RandomQueue<Card> randomQueue = RandomQueue.RandomQueue();
 
+        // enqueue 13 times, dequeue 13 times, enqueue 13 times, sample 4 times
+        test1(randomQueue, mixedDeck);
+
+        // enqueue 4, dequeue 2, sample 2, enqueue 11, dequeue 7, sample 3
+        test2(randomQueue, mixedDeck);
+    }
+
+    private static void test1 (RandomQueue<Card> randomQueue, MixedDeck mixedDeck){
+        StdOut.println("Test 1: ");
         enqueue(13, randomQueue, mixedDeck);
         dequeue(13, randomQueue);
 
@@ -10,19 +19,30 @@ public class Exercise1335_RandomQueue {
         sample(4, randomQueue);
     }
 
+    private static void test2 (RandomQueue<Card> randomQueue, MixedDeck mixedDeck){
+        StdOut.println("Test 2: ");
+        enqueue(4, randomQueue, mixedDeck);
+        dequeue(2, randomQueue);
+        sample(2, randomQueue);
+        enqueue(11, randomQueue, mixedDeck);
+        dequeue(7, randomQueue);
+        sample(3, randomQueue);
+    }
+
     private static void enqueue(int times, RandomQueue<Card> randomQueue, MixedDeck mixedDeck) {
-        StdOut.println("Enqueue " + times + " times");
+        StdOut.println("Enqueue " + times + " times ");
+        StdOut.println();
         for (int i = 0; i < times; i++) {
             randomQueue.enqueue(mixedDeck.getCard());
         }
-        StdOut.println(times + " card(s) Deck: ");
+        StdOut.println(times + " card(s) bridge hand: ");
         printRandomQueue(randomQueue);
         StdOut.println();
     }
 
     private static void dequeue(int times, RandomQueue<Card> randomQueue) {
         for (int i = 0; i < times; i++) {
-            StdOut.println("Dequeued card: " + randomQueue.dequeue().getCard() + ", deck left: ");
+            StdOut.println("Dequeued card: " + randomQueue.dequeue().getCard() + ", bridge hand left: ");
             StdOut.println();
             printRandomQueue(randomQueue);
             StdOut.println();
@@ -31,7 +51,7 @@ public class Exercise1335_RandomQueue {
 
     private static void sample(int times, RandomQueue<Card> randomQueue) {
         for (int i = 0; i < times; i++) {
-            StdOut.println("Sample: " + randomQueue.sample().getCard());
+            StdOut.println("Sample: " + randomQueue.sample().getCard() + ", brigde hand now is: ");
             StdOut.println();
             printRandomQueue(randomQueue);
             StdOut.println();
---
+++ Exercise1336_RandomQueueRandomIterator.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class Exercise1336_RandomQueueRandomIterator {
+}
