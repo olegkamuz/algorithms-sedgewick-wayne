f1d1f8c94ddaaa07c6b3a8cee2ab0a0f8ad95a3a
olegbc <olegkamuz@gmail.com>
2018-03-09 00:20:39 +0200

    Exercise1336_RandomQueueRandomIterator

--- Exercise1336_RandomQueueRandomIterator.java
+++ Exercise1336_RandomQueueRandomIterator.java
@@ -1,4 +1,71 @@
-package PACKAGE_NAME;
-
 public class Exercise1336_RandomQueueRandomIterator {
+    public static void main(String[] args) {
+        MixedDeck mixedDeck = MixedDeck.MixedDeck();
+        RandomQueueRandomIterator<Card> randomQueue = RandomQueueRandomIterator.RandomQueueRandomIterator();
+
+        // enqueue 13 times, dequeue 13 times, enqueue 13 times, sample 4 times
+        test1(randomQueue, mixedDeck);
+
+        // enqueue 4, dequeue 2, sample 2, enqueue 11, dequeue 7, sample 3
+        test2(randomQueue, mixedDeck);
+    }
+
+    private static void test1 (RandomQueueRandomIterator<Card> randomQueue, MixedDeck mixedDeck){
+        StdOut.println("Test 1: ");
+        enqueue(13, randomQueue, mixedDeck);
+        dequeue(13, randomQueue);
+
+        enqueue(13, randomQueue, mixedDeck);
+        sample(4, randomQueue);
+    }
+
+    private static void test2 (RandomQueueRandomIterator<Card> randomQueue, MixedDeck mixedDeck){
+        StdOut.println("Test 2: ");
+        enqueue(4, randomQueue, mixedDeck);
+        dequeue(2, randomQueue);
+        sample(2, randomQueue);
+        enqueue(11, randomQueue, mixedDeck);
+        dequeue(7, randomQueue);
+        sample(3, randomQueue);
+    }
+
+    private static void enqueue(int times, RandomQueueRandomIterator<Card> randomQueue, MixedDeck mixedDeck) {
+        StdOut.println("Enqueue " + times + " times ");
+        StdOut.println();
+        for (int i = 0; i < times; i++) {
+            randomQueue.enqueue(mixedDeck.getCard());
+        }
+        StdOut.println(times + " card(s) bridge hand: ");
+        printRandomQueue(randomQueue);
+        StdOut.println();
+    }
+
+    private static void dequeue(int times, RandomQueueRandomIterator<Card> randomQueue) {
+        for (int i = 0; i < times; i++) {
+            StdOut.println("Dequeued card: " + randomQueue.dequeue().getCard() + ", bridge hand left: ");
+            StdOut.println();
+            printRandomQueue(randomQueue);
+            StdOut.println();
+        }
+    }
+
+    private static void sample(int times, RandomQueueRandomIterator<Card> randomQueue) {
+        for (int i = 0; i < times; i++) {
+            StdOut.println("Sample: " + randomQueue.sample().getCard() + ", brigde hand now is: ");
+            StdOut.println();
+            printRandomQueue(randomQueue);
+            StdOut.println();
+        }
+    }
+
+    private static void printRandomQueue(RandomQueueRandomIterator<Card> randomQueue) {
+        if (randomQueue.isEmpty()) {
+            StdOut.println("Deck is empty");
+        } else {
+            for (Card s : randomQueue) {
+                StdOut.println(s.getCard());
+            }
+        }
+    }
 }
+
--- RandomQueueRandomIterator.java
+++ RandomQueueRandomIterator.java
@@ -1,4 +1,127 @@
-package PACKAGE_NAME;
+import java.util.Iterator;
 
-public class RandomQueueRandomIterator {
+public class RandomQueueRandomIterator<Item> implements RandomQueueAPI<Item> {
+    private int size;
+    private Item[] arr;
+    private int sizeCopy;
+    private Item[] arrCopy;
+
+    @SuppressWarnings("unchecked")
+    public static RandomQueueRandomIterator RandomQueueRandomIterator() {
+        return new RandomQueueRandomIterator();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize(int max) {
+        Item[] temp = (Item[]) new Object[max];
+        for (int i = 0; i < size; i++) {
+            temp[i] = arr[i];
+            arr[i] = null;
+        }
+        arr = temp;
+    }
+
+    @SuppressWarnings("unchecked")
+    public RandomQueueRandomIterator() {
+        arr = (Item[]) new Object[1];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void enqueue(Item item) {
+        if (size + 1 >= arr.length) {
+            resize(arr.length * 2);
+        }
+        size++;
+        arr[size - 1] = item;
+    }
+
+    @SuppressWarnings("unchecked")
+    public Item dequeue() {
+        if (isEmpty()) return null;
+        if (size - 1 <= arr.length / 4) {
+            resize(arr.length / 2);
+        }
+        int key = StdRandom.uniform(size);
+        Item item = arr[key];
+        arr[key] = null;
+        for (int i = key; i < size - 1; i++) {
+            if (arr[i + 1] != null) {
+                arr[i] = arr[i + 1];
+                arr[i + 1] = null;
+            }
+        }
+        size--;
+        return item;
+    }
+
+    public int arrSize() {
+        return arr.length;
+    }
+
+    @SuppressWarnings("unchecked")
+    public Item sample() {
+        int key = StdRandom.uniform(size);
+        Item item = arr[key];
+        for (int i = key; i < size; i++) {
+            if (arr[i + 1] != null && i < size) {
+                arr[i] = arr[i + 1];
+            }
+            if (i == size - 1) {
+                arr[i] = item;
+            }
+        }
+        return item;
+    }
+
+    public RandomQueueRandomIterator<Item> clone() {
+        RandomQueueRandomIterator<Item> copy = RandomQueueRandomIterator.RandomQueueRandomIterator();
+        for (int i = 0; i < arr.length; i++) {
+            copy.enqueue(arr[i]);
+        }
+        return copy;
+    }
+
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        int current;
+        public ListIterator() {
+            super();
+            arrCopy = (Item[]) new Object[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                arrCopy[i] = arr[i];
+            }
+            sizeCopy = size;
+        }
+
+        public boolean hasNext() {
+            if(sizeCopy == 0) return false;
+            current = StdRandom.uniform(sizeCopy);
+            return arrCopy[current] != null;
+        }
+        public Item next() {
+            Item item = arrCopy[current];
+            for (int i = current; i < sizeCopy; i++) {
+                if (arrCopy[i + 1] != null) {
+                    arrCopy[i] = arrCopy[i + 1];
+                    arrCopy[i + 1] = null;
+                }
+            }
+            sizeCopy--;
+            return item;
+        }
+
+        public void remove() {
+        }
+    }
 }
+
