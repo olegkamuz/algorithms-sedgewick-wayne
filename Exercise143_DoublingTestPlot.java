1e4b1e026e7f6cdaeea2e67bfb0a62b930ee7053
olegkamuz <olegkamuz@gmail.com>
2018-05-11 17:01:53 +0300

    Update Exercise143_DoublingTestPlot Cached

--- Exercise143_DoublingTestPlot.java
+++ Exercise143_DoublingTestPlot.java
@@ -1,5 +1,9 @@
 import java.io.IOException;
 
+/**
+ * Visualisation: https://www.youtube.com/watch?v=V76D9ClMx3g
+ */
+
 public class Exercise143_DoublingTestPlot {
     public static void main(String[] args) throws InterruptedException, IOException {
         int numberOfTests = 4; // same as number point at curve
