eac9a833560d128d40d49683ad89aacc58189011
olegkamuz <olegkamuz@gmail.com>
2019-03-16 23:26:49 +0200

    Exercise1417_FarthestPair

---
+++ Exercise1417_FarthestPair.java
@@ -0,0 +1,6 @@
+public class Exercise1417_FarthestPair {
+    public static void main(String[] args) {
+        double[] a = (new RandomArray()).getRandomArray(10, -10.0, 10.0);
+        new FarthestPair(a);
+    }
+}
